c24b861144f23f772826f16d4716718dc51d6620
Jakalgit <mistetsmit05@gmail.com>
2024-11-30 13:42:59 +0300

    comm

--- src/components/CatalogItem.js
+++ src/components/CatalogItem.js
@@ -49,7 +49,7 @@ const CatalogItem = (props) => {
                 </div>
                 <div className={ItemCss.img + ' col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'}>
                     {props.item.filename ?
-                        <img src={`${process.env.IMAGE_URL}/static/image/${props.item.filename}`} alt="" className={ItemCss.item_image} onClick={itemClick}/>
+                        <img src={`http://195.133.30.5:5000/static/image/${props.item.filename}`} alt="" className={ItemCss.item_image} onClick={itemClick}/>
                         :
                         <div className={ItemCss.item_image}/>
                     }
--- src/pages/item/[id].js
+++ src/pages/item/[id].js
@@ -108,7 +108,7 @@ function ItemPage({ itemServer, title }) {
                                     {itemServer.images.map(image =>
                                         <Carousel.Item>
                                             <div className={style_css.img}>
-                                                <img src={`${process.env.IMAGE_URL}/static/image/${image.filename}`} alt="" className={style_css.image} />
+                                                <img src={`http://195.133.30.5:5000/static/image/${image.filename}`} alt="" className={style_css.image} />
                                             </div>
                                         </Carousel.Item>
                                     )}
