6c46bf301afd4856c08ee8fbb3058784209904a2
misha <mistetsmit05@gmail.com>
2025-03-16 17:09:26 +0300

    commit

--- src/http/index.js
+++ src/http/index.js
@@ -2,6 +2,9 @@ import axios from 'axios'
 
 const $host = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    headers: {
+        "tuna-skip-browser-warning": "skip",
+    }
 })
 
 export {
