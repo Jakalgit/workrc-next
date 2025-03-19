e991e153a5ee32d792af10de7f79bcd11c387c75
misha <mistetsmit05@gmail.com>
2025-03-19 11:27:32 +0300

    commit

--- src/pages/your-order/[number].js
+++ src/pages/your-order/[number].js
@@ -6,7 +6,6 @@ import general from "@/css/General.module.css"
 import YourOrderItem from "@/components/YourOrderItem";
 import Footer from "@/components/Footer";
 import {Spinner} from "react-bootstrap";
-import {Bounce, Fade, Slide, Flip} from "react-reveal";
 import Head from "next/head";
 import {useRouter} from "next/router";
 
@@ -72,17 +71,15 @@ function YourOrder() {
                         <div className="container">
                             <div className="row">
                                 <h2 className={style_css.check_order_text + ' col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'}>
-                                    <Bounce cascade>
-                                        {'Заказ № ' + order.number}
-                                    </Bounce>
+                                    {'Заказ № ' + order.number}
                                 </h2>
                                 <div className={style_css.left_block + ' col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12'}>
-                                    <Fade top>
+                                    <div>
                                         <h2 className={style_css.header_info}>ФИО:</h2>
                                         <h2 className={style_css.info}>{(order.lastName === 'не указано' ? '' : order.lastName) + ' ' +
                                             order.firstName + ' ' + (order.secondName === 'не указано' ? '' : order.secondName)}</h2>
-                                    </Fade>
-                                    <Fade top>
+                                    </div>
+                                    <div>
                                         <h2 className={style_css.header_info}>Контактные данные:</h2>
                                         {order.phoneNumber ?
                                             <h2 className={style_css.info}>{order.phoneNumber}</h2>
@@ -94,8 +91,8 @@ function YourOrder() {
                                             :
                                             <h2 className={style_css.info}>Адрес почты: не указано</h2>
                                         }
-                                    </Fade>
-                                    <Fade top>
+                                    </div>
+                                    <div>
                                         {order.typeDelivery === '1' ?
                                             <div/>
                                             :
@@ -114,18 +111,18 @@ function YourOrder() {
                                                 }
                                             </div>
                                         }
-                                    </Fade>
+                                    </div>
                                 </div>
                                 <div className={style_css.right_block + ' col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12'}>
-                                    <Fade top>
+                                    <div>
                                         <h2 className={style_css.header_info}>Способ оплаты:</h2>
                                         {order.typePay === '1' ?
                                             <h2 className={style_css.info}>Оплата онлайн</h2>
                                             :
                                             <h2 className={style_css.info}>Оплата наличными</h2>
                                         }
-                                    </Fade>
-                                    <Fade top>
+                                    </div>
+                                    <div>
                                         <h2 className={style_css.header_info}>Способ доставки:</h2>
                                         {order.typeDelivery === '1' ?
                                             <h2 className={style_css.info}>Самовывоз</h2>
@@ -135,46 +132,40 @@ function YourOrder() {
                                                 :
                                                 <h2 className={style_css.info}>{order.house}</h2>
                                         }
-                                    </Fade>
-                                    <Fade top>
+                                    </div>
+                                    <div>
                                         {order.track === 'не указано' ?
                                             <h2 className={style_css.info}>------------------------</h2>
                                             :
                                             <h2 className={style_css.info}>{'Трек-номер: ' + order.track}</h2>
                                         }
-                                    </Fade>
-                                    <Fade top>
+                                    </div>
+                                    <div>
                                         <h2 className={style_css.header_info}>Статус заказа:</h2>
                                         <h2 className={style_css.info}>{order.typeSubmit}</h2>
-                                    </Fade>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
-                    <Slide right>
-                        {order.track === 'не указано' && order.typeDelivery === '3' ?
-                            <div className="order-info">
-                                <div className="conatiner">
-                                    <div className="row">
-                                        <h2 className={style_css.info + ' ' + style_css.margin + ' col-xxl-10 offset-xxl-1 col-xl-10 offset-xl-1 col-lg-10 offset-lg-1 col-md-10 offset-md-1 col-sm-10 offset-sm-1 col-10 offset-1'}>Как
-                                            только ваш заказ будет передан в службу СДЭК в пункте <strong>"Способ
-                                                доставки"</strong> появится трек-номер посылки.</h2>
-                                    </div>
+                    {order.track === 'не указано' && order.typeDelivery === '3' ?
+                        <div className="order-info">
+                            <div className="conatiner">
+                                <div className="row">
+                                    <h2 className={style_css.info + ' ' + style_css.margin + ' col-xxl-10 offset-xxl-1 col-xl-10 offset-xl-1 col-lg-10 offset-lg-1 col-md-10 offset-md-1 col-sm-10 offset-sm-1 col-10 offset-1'}>Как
+                                        только ваш заказ будет передан в службу СДЭК в пункте <strong>"Способ
+                                            доставки"</strong> появится трек-номер посылки.</h2>
                                 </div>
                             </div>
-                            :
-                            <div/>
-                        }
-                    </Slide>
-
+                        </div>
+                        :
+                        <div/>
+                    }
                     <div className="item-block">
                         <div className="container">
                             <div className="row">
                                 <h2 className={style_css.order_sum}>
-                                    <Flip right cascade>
-                                        <p className={style_css.text}>Сумма заказа:</p>{' ' + fullPrice + ' ₽'}
-                                    </Flip>
+                                    <p className={style_css.text}>Сумма заказа:</p>{' ' + fullPrice + ' ₽'}
                                 </h2>
                                 {orderItems.length !== 0 ?
                                     <div className="items">
@@ -195,6 +186,6 @@ function YourOrder() {
             }
         </div>
     );
-};
+}
 
 export default YourOrder;
